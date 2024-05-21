d2778521321de0f534ad33578209c57f08f0dbf5
Chad Allen <chadallenmagic@gmail.com>
2024-05-20 17:03:27 -0700

    updated homework assignment two

---
+++ main.js
@@ -0,0 +1,6 @@
+const now = new Date();
+
+// get the current date and time as a string
+const currentDateTime = now.toLocaleString();
+
+console.log(currentDateTime); // output: "7/20/2021, 2:28:15 PM" (will vary depending on your time zone)
